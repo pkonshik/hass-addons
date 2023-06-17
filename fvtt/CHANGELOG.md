73ef6724c4b793c08579770e10790c7d4c61d521
pkonshik <52590783+pkonshik@users.noreply.github.com>
2023-06-17 15:12:58 +0300

    v1.0.2 - fvtt addon (fvtt 11.301.0) (#17)
    Added bashio
    Updated fvtt base image to 11.300.1 version
    added configurable variables
    adjust image url

--- fvtt/CHANGELOG.md
+++ fvtt/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.2
+- Rebased base image to the original felddy/foundryvtt image
+- Added bashio support
+- Added configurable values instead of ENV
 ## 1.0.1
 - Added preserve config flag for fvtt addon
 ## 1.0.0
---
+++ fvtt/init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bashio
+
+CONTAINER_PRESERVE_CONFIG=$(bashio::config 'container_preserve_config')
+bashio::log.info "CONTAINER_PRESERVE_CONFIG = ${CONTAINER_PRESERVE_CONFIG}"
+FOUNDRY_VTT_DATA_PATH=$(bashio::config 'foundry_vtt_data_path')
+bashio::log.info "FOUNDRY_VTT_DATA_PATH = ${FOUNDRY_VTT_DATA_PATH}"
+CONTAINER_CACHE=$(bashio::config 'container_cache')
+bashio::log.info "CONTAINER_CACHE = ${CONTAINER_CACHE}"
+FOUNDRY_SSL_CERT=$(bashio::config 'certfile')
+bashio::log.info "FOUNDRY_SSL_CERT = ${FOUNDRY_SSL_CERT}"
+FOUNDRY_SSL_KEY=$(bashio::config 'keyfile')
+bashio::log.info "FOUNDRY_SSL_KEY = ${FOUNDRY_SSL_KEY}"
+
+export CONTAINER_PRESERVE_CONFIG FOUNDRY_VTT_DATA_PATH CONTAINER_CACHE FOUNDRY_SSL_CERT FOUNDRY_SSL_KEY
+
+bashio::log.info "Calling native entrypoint with the following arguments" "$@" "--dataPath=${FOUNDRY_VTT_DATA_PATH}"
+./entrypoint.sh "$@" "--dataPath=${FOUNDRY_VTT_DATA_PATH}"
